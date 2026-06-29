528c108e8ff490f470b935dfab85cbd543229e81
Kak0ytachel <alesha.belarus@gmail.com>
2026-06-29 21:19:11 +0200

    partially implemented transformOptimal

--- src/transformers/optimalTransformer.ts
+++ src/transformers/optimalTransformer.ts
@@ -1,4 +1,29 @@
+import type {IGenerationData, IGenerationResponse, ICleanEnergyData, IEnergyTypeItem} from "../types.js";
 
-function transformOptimal() {
+function transformOptimal(payload: IGenerationResponse, chargeHours: number) {
+    const data: IGenerationData[] = payload.data.slice(1);
+
+    const cleanEnergyData: ICleanEnergyData[] = [];
+    for (let elem of data) {
+        const items: IEnergyTypeItem[] = elem.generationmix;
+        let cleanPerc: number = 0;
+
+        for (let item of items) {
+            if (item.fuel in ["biomass", "nuclear", "hydro", "wind", "solar"]) {
+                cleanPerc += item.perc;
+            }
+        }
+
+        cleanEnergyData.push(
+            {from: elem.from, to: elem.to, perc: cleanPerc}
+        )
+    }
+
+    const chargeSegments = chargeHours * 2;
+
+    for (let i = 0; i < cleanEnergyData.length - chargeSegments; i++) {
+        // todo
+    }
+
 
 }
--- src/types.ts
+++ src/types.ts
@@ -9,12 +9,27 @@ export interface IGenerationData {
     from: string;
     to: string;
     //noinspection SpellCheckingInspection
-    generationmix: Array<IGenerationMixItem>
+    generationmix: Array<IEnergyTypeItem>
 }
 
 
-export interface IGenerationMixItem {
-    fuel: string,
+export interface IEnergyTypeItem {
+    fuel: string, // FuelType,
     //noinspection SpellCheckingInspection
+    perc: number // left as "perc" to match API
+}
+
+// export type FuelType = "gas" | "coal" | "biomass" | "nuclear" | "hydro" | "imports" | "other" | "wind" | "solar";
+
+export interface ICleanEnergyData {
+    from: string,
+    to: string,
+    //noinspection SpellCheckingInspection
+    perc: number // left as "perc" to match other types
+}
+
+export interface ICleanEnergyInterval {  // made as a separate interface for future upgradability
+    from: string,
+    to: string,
     perc: number
 }
