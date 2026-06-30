a33382c822a9301a4a0382e8b3bf63fedf8b5673
Kak0ytachel <alesha.belarus@gmail.com>
2026-06-30 19:05:57 +0200

    added querystring validation

--- src/routes/optimal.ts
+++ src/routes/optimal.ts
@@ -1,7 +1,24 @@
-import type {FastifyInstance} from "fastify";
+import type {FastifyInstance, RouteShorthandOptions} from "fastify";
+
+
+const opts: RouteShorthandOptions = {
+    schema: {
+        querystring: {
+            type: 'object',
+            properties: {
+                chargeHours: {
+                    type: 'integer',
+                    minimum: 1,
+                    maximum: 6,
+                }
+            },
+            required: ['chargeHours']
+        }
+    }
+}
 
 export default function optimalRoute(fastify: FastifyInstance, options: Object) {
-    fastify.get('/optimal', async (request, reply) => {
+    fastify.get('/optimal', opts, async (request, reply) => {
         return 'Hello World!!!!\n'
     })
 }
