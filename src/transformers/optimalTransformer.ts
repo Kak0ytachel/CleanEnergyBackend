6b5d8987eb711063655b0cfc443905d9950401e6
Kak0ytachel <alesha.belarus@gmail.com>
2026-06-30 11:48:10 +0200

    optimal transformer implemented

--- src/transformers/optimalTransformer.ts
+++ src/transformers/optimalTransformer.ts
@@ -1,6 +1,12 @@
-import type {IGenerationData, IGenerationResponse, ICleanEnergyData, IEnergyTypeItem} from "../types.js";
+import type {
+    IGenerationData,
+    IGenerationResponse,
+    ICleanEnergyData,
+    IEnergyTypeItem,
+    ICleanEnergyInterval
+} from "../types.js";
 
-function transformOptimal(payload: IGenerationResponse, chargeHours: number) {
+function transformOptimal(payload: IGenerationResponse, chargeHours: number): ICleanEnergyInterval {
     const data: IGenerationData[] = payload.data.slice(1);
 
     const cleanEnergyData: ICleanEnergyData[] = [];
@@ -19,11 +25,32 @@ function transformOptimal(payload: IGenerationResponse, chargeHours: number) {
         )
     }
 
-    const chargeSegments = chargeHours * 2;
+    const cleanEnergyIntervals: ICleanEnergyInterval[] = [];
 
-    for (let i = 0; i < cleanEnergyData.length - chargeSegments; i++) {
-        // todo
+    const chargeSegments: number = chargeHours * 2;
+    for (let startIndex = 0; startIndex < cleanEnergyData.length - chargeSegments; startIndex++) {
+        const startTime: string = cleanEnergyData[startIndex]!.from
+        const endTime: string = cleanEnergyData[startIndex + chargeSegments - 1]!.from
+        let percentageSum: number = 0;
+        for (let i = 0; i < chargeSegments; i++) {
+            const el: ICleanEnergyData = cleanEnergyData[startIndex + i]!;
+            percentageSum += el.perc;
+        }
+        const averagePercentage: number = percentageSum / chargeSegments;
+
+        cleanEnergyIntervals.push(
+            {from: startTime, to: endTime, perc: averagePercentage}
+        )
     }
+
+    const getCleanerIntervals = (a: ICleanEnergyInterval, b: ICleanEnergyInterval): ICleanEnergyInterval => {
+        return a.perc >= b.perc ? a : b;
+    }
+
+    const cleanestInterval = cleanEnergyIntervals.reduce(getCleanerIntervals)
+    // console.log(cleanestInterval);
+
+    return cleanestInterval;
 
 
 }
