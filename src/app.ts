375f7f655984c4b534c4eb25a19b6dbaa92bbc68
Kak0ytachel <alesha.belarus@gmail.com>
2026-06-30 19:56:58 +0200

    added pino-pretty and vitest

--- src/app.ts
+++ src/app.ts
@@ -2,7 +2,14 @@ import fastify from "fastify";
 import routes from "./routes/index.ts";
 
 export default function buildApp() {
-    const app = fastify({logger: true});
+    const app = fastify({
+        logger: {
+            transport: {
+                target: 'pino-pretty'
+            },
+            level: 'debug',
+            },
+        });
     app.register(routes)
     return app;
 }
